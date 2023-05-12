23989880a31c59c68f4c231fa056d6027d6349f8
Naomi Peña <naosan25@protonmail.com>
2023-05-11 18:25:30 -0600

    feat: typing

---
+++ src/Components/staticComponents/Typings.d.ts
@@ -0,0 +1,17 @@
+// archivo para importar y exportar las interfaces y el tipado
+
+ export type NoteType = { // voy a recibir esto como prop dentro del componente Note
+    text: string
+    isEditing: boolean
+  }; 
+
+  export type NbProps = { 
+    logo: string;
+    buttonLabels: ButtonLabel[];
+  }; // Update the NbProps type to use ButtonLabel[]
+
+  
+export type ButtonLabel = {
+  label: string; // because of the URL
+  className: string; 
+}; // Defines type for the button labels
