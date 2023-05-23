a2a60f51cc4e6d429334d1b535639beacff382cd
Naomi Peña <naosan25@protonmail.com>
2023-05-23 17:16:28 -0600

    chore: firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,26 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore, collection, getDocs, getDoc } from 'firebase/firestore';
 
 const firebaseApp = initializeApp({
     apiKey: "{}",
     authDomain: "{}",
     projectId: "{}",
-})
+});
 
 const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
+db.collection('todos').getDocs();
+const todosCol = collection(db, 'todos');
+const snapshot = await getDocs(todosCol);
+
+onAuthStateChanged(auth, user => {
+    if (user ≠ null) {
+    console.log('Log in succesful!');
+} else {
+    console.log('Authentication error');
+}
+});
 
 const firebaseConfig = {
     apiKey: "{}",
